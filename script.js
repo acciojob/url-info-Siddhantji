136f7545b529a639cd9845dd673896d7514203df
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-05 20:55:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //your JS code here. If required.
-const url = location.location.href;
+// Get the current URL using location.href
+const currentURL = location.href;
+
+// Get the length of the URL string
+const urlLength = currentURL.length;
+
+// Display the length of the URL using alert()
 alert(`The length of the URL is: ${urlLength}`);
